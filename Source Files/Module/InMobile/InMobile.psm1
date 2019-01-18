282a973251180ec1c6215b83e2c704efbdc899c3
Daniel Lindegaard <Latupho@users.noreply.github.com>
2019-01-18 14:54:49 +0100

    The first steps to read the XML from InMobile, plus
    The frams of the module...
    
    Created a way (PoC) to read the XML so it's showen in Objects in PS...
    WIll need to make a function that read this XML data from InMobile, so it's allwaysed showen as Objects.

---
+++ Source Files/Module/InMobile/InMobile.psm1
@@ -0,0 +1,197 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.156
+	 Created on:   	18-01-2019 11:07
+	 Created by:   	Daniel Lindegaard
+	 Organization: 	
+	 Filename:     	InMobile.psm1
+	-------------------------------------------------------------------------
+	 Module Name: InMobile
+	===========================================================================
+#>
+
+
+<#
+	.SYNOPSIS
+		A brief description of the New-ConfigFile function.
+
+	.DESCRIPTION
+		A detailed description of the New-ConfigFile function.
+
+	.PARAMETER  ParameterA
+		The description of a the ParameterA parameter.
+
+	.PARAMETER  ParameterB
+		The description of a the ParameterB parameter.
+
+	.EXAMPLE
+		PS C:\> New-ConfigFile -ParameterA 'One value' -ParameterB 32
+		'This is the output'
+		This example shows how to call the New-ConfigFile function with named parameters.
+
+	.EXAMPLE
+		PS C:\> New-ConfigFile 'One value' 32
+		'This is the output'
+		This example shows how to call the New-ConfigFile function with positional parameters.
+
+	.INPUTS
+		System.String,System.Int32
+
+	.OUTPUTS
+		System.String
+
+	.NOTES
+		For more information about advanced functions, call Get-Help with any
+		of the topics in the links listed below.
+
+	.LINK
+		about_functions_advanced
+
+	.LINK
+		about_comment_based_help
+
+	.LINK
+		about_functions_advanced_parameters
+
+	.LINK
+		about_functions_advanced_methods
+#>
+function New-ConfigFile {
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0, Mandatory=$false)]
+		[System.Boolean]
+		$Test = $True,
+		[Parameter(Position=1, Mandatory = $True)]
+		[System.String]
+		$Path
+	)
+	begin {
+		try
+		{
+			#region HashTables Chunks.
+			<#
+			In this region there are diffrent Regions of the Config file. It's been
+			created in HashTables, and then assembelt in main Hash that will be outputet.
+			#>
+			[hashtable]$TestGroups_Locations = @{
+				Header		   		= "TestGroups_Locations";
+				Locations_GroupName = "DataStuff";
+			}
+			[hashtable]$ProdGroups_Locations = @{
+				Header		   		= "ProdGroups_Locations";
+				Locations_GroupName = "Locations_Prod"; }
+			
+			[hashtable]$Locations_RoomNr = @{
+				Header		   = "Room Numbers";
+				Locations_Test = "Datastuff"; }
+			
+			#endregion
+			
+			#region Main HashTables constructs.
+						
+			[hashtable]$exampleConfig = @{
+				Header0   = "Datastuff";
+				Locations = $Locations;
+			}
+			
+			#endregion
+			
+		}
+		catch
+		{
+			
+		}
+	}
+	process {
+		try {
+			$TestGroups_Locations | ConvertTo-Json | clip
+		}
+		catch {
+		}
+	}
+	end {
+		try {
+		}
+		catch {
+		}
+	}
+}
+
+<#
+	.SYNOPSIS
+		A brief description of the Get-GroupData function.
+
+	.DESCRIPTION
+		A detailed description of the Get-GroupData function.
+
+	.PARAMETER  ParameterA
+		The description of a the ParameterA parameter.
+
+	.PARAMETER  ParameterB
+		The description of a the ParameterB parameter.
+
+	.EXAMPLE
+		PS C:\> Get-GroupData -ParameterA 'One value' -ParameterB 32
+		'This is the output'
+		This example shows how to call the Get-GroupData function with named parameters.
+
+	.EXAMPLE
+		PS C:\> Get-GroupData 'One value' 32
+		'This is the output'
+		This example shows how to call the Get-GroupData function with positional parameters.
+
+	.INPUTS
+		System.String,System.Int32
+
+	.OUTPUTS
+		System.String
+
+	.NOTES
+		For more information about advanced functions, call Get-Help with any
+		of the topics in the links listed below.
+
+	.LINK
+		about_functions_advanced
+
+	.LINK
+		about_comment_based_help
+
+	.LINK
+		about_functions_advanced_parameters
+
+	.LINK
+		about_functions_advanced_methods
+#>
+function Get-GroupData {
+	[CmdletBinding()]
+	param(
+		[Parameter(Position=0, Mandatory=$true)]
+		[System.String]
+		$API,
+		[Parameter(Position=1)]
+		[System.Int32]
+		$ParameterB
+	)
+	begin {
+		try
+		{
+			$DataSet000 = Invoke-WebRequest -Body $MultipartContent -Method 'POST' -Uri "https://mm.inmobile.dk/Api/V3/Groups/GetAll?apiKey=$($API.trim())"
+		}
+		catch {
+		}
+	}
+	process {
+		try {
+			$DataSet000.content
+		}
+		catch {
+		}
+	}
+	end {
+		try {
+		}
+		catch {
+		}
+	}
+}
---
+++ Source Files/Module/InMobile/Test-Module.ps1
@@ -0,0 +1,51 @@
+﻿<#	
+	.NOTES 
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.156
+	 Created on:   	18-01-2019 11:07
+	 Created by:   	Daniel Lindegaard
+	 Organization: 	
+	 Filename:     	Test-Module.ps1
+	===========================================================================
+	.DESCRIPTION
+	The Test-Module.ps1 script lets you test the functions and other features of
+	your module in your PowerShell Studio module project. It's part of your project,
+	but it is not included in your module.
+
+	In this test script, import the module (be careful to import the correct version)
+	and write commands that test the module features. You can include Pester
+	tests, too.
+
+	To run the script, click Run or Run in Console. Or, when working on any file
+	in the project, click Home\Run or Home\Run in Console, or in the Project pane, 
+	right-click the project name, and then click Run Project.
+#>
+
+$ScriptLocation	= Split-Path -Path ($MyInvocation.MyCommand.Path | Get-ItemProperty | Select-Object -ExpandProperty DirectoryName)
+$Config			= (ConvertFrom-Json -InputObject $(Get-Content -Path "$((Get-Item -Path $ScriptLocation).Parent)\Config\_NoComit_Config_Test.json" -Raw -Encoding UTF8))
+$API			= $Config.System_Information.InMobile_API
+
+#Explicitly import the module for testing
+Import-Module 'InMobile'
+
+#Run each module function
+Get-GroupData -API $API | Out-File -PSPath "$ScriptLocation\XMLDataFromInMobile.xml"
+[XML]$FromInMobile = Get-Content "$ScriptLocation\XMLDataFromInMobile.xml"
+
+$FromInMobile.ApiResult.Data.Group.Id
+
+foreach ($Group in ($FromInMobile.ApiResult.Data.Group)) {
+	$GroupsObj = @{
+		GroupeName = $Group.Name;
+		GroupeID = $Group.Id
+	}
+	$object = new-object psobject -Property $GroupsObj
+	$object
+}
+
+#Sample Pester Test
+#Describe "Test InMobile" {
+#	It "tests Write-HellowWorld" {
+#		Write-HelloWorld | Should BeExactly "Hello World"
+#	}	
+#}
